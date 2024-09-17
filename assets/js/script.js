c6abc9b6fbd530b502fd2987d06bb107a7f482e3
Luis Angelis <luisangelis.ti@gmail.com>
2024-09-17 12:28:44 -0300

    bug dasboard fixed

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-const btnMenu = document.getElementById('btn-menu');
-const sidebar = document.getElementById('sidebar');
-
-// Evento de clique no botão para alternar a classe "hidden"
-btnMenu.addEventListener('click', function(){
-    sidebar.classList.toggle('hidden');
-});  
